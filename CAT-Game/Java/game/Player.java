96a597c941939a408699c62e8e675e57a726c297
dlkohler <dkohler42@gmail.com>
2017-05-19 15:53:29 -0700

    656389 - updated player character class

--- CAT-Game/Java/game/Player.java
+++ CAT-Game/Java/game/Player.java
@@ -13,7 +13,29 @@ public class Player extends PhysObject {
     */
    public Player(int posX, int posY) {
       super(posX, posY);
+      angle = 0;
    }
 
+   // -------------------------------------------------------------------------
 
+   private void calculateAngle() {
+      double tempAngle = tan((mouseY - position.y)/(mouseX-position.x));
+      while (tempAngle < 0) {
+         tempAngle += TWO_PI;
+      }
+      while (tempAngle > TWO_PI) {
+         tempAngle -= TWO_PI;
+      }
+      angle = addAngle(tempAngle);
+   }
+
+   private void calculateMovement() {
+
+   }
+
+   public void draw() {
+      // calculate new angle based on mouse location
+      calculateAngle();
+      calculateMovement();
+   }
 }
