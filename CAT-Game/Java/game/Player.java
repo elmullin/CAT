49d92c46ff54de5a735ff16b1694814dbc3af3b0
dlkohler <dkohler42@gmail.com>
2017-05-19 08:26:27 -0700

    656389 - started player character class

---
+++ CAT-Game/Java/game/Player.java
@@ -0,0 +1,19 @@
+package game;
+
+import processing.core.*;
+
+public class Player extends PhysObject {
+   /*
+    * Fields
+    */
+   private double angle;
+
+   /**
+    * Constructor. Creates a new object at the given location.
+    */
+   public Player(int posX, int posY) {
+      super(posX, posY);
+   }
+
+
+}
