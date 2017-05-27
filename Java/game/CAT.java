cecf93eda2afbcb51f7b07cc1627d32efb4118cd
LiamGow <liam.m.gow@gmail.com>
2017-05-27 14:48:44 -0700

    #662686 - added java file versions

--- Java/game/CAT.java
+++ Java/game/CAT.java
@@ -4,19 +4,19 @@ import processing.core.*;
 
 public class CAT extends PApplet{
 
-	private Background bg;
-	
-	public void settings(){
-	    size(800, 600);
-	}
-	
-	public void setup(){
-	    //scene setup
-	    surface.setResizable(false);
-	    bg = new Background("bg.png");
-	}
-	
-	public void draw(){
-	    bg.display();
-	}
+private World world;
+
+public void settings(){
+    world.applySettings();
+}
+
+public void setup(){
+    //scene setup
+    surface.setResizable(false);
+    world = new World(new Background("bg.png"), 800, 600);
+    world.addActor(new TabbyCat(50, 50));
+}
+
+public void draw(){
+    world.display();
 }
--- Java/game/World.java
+++ Java/game/World.java
@@ -4,51 +4,87 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class World extends processing.core.PApplet {
-	  ArrayList<Actor> actors;
-	  
-	  static int defaultWidth = 620;
-	  static int defaultHeight = 480;
-	  
-	  int tempWidth = defaultWidth;
-	  int tempHeight = defaultHeight;
-	  
-	  public World() {
-	    actors = new ArrayList<>();
-	  }
-	  
-	  public World(int width, int height) {
-	    actors = new ArrayList<>();
-	    
-	    tempWidth = width;
-	    tempHeight = height;
-	  }
-	  
-	  @Override
-	  public void draw() {
-	    for (Actor actor : actors) {
-	      actor.draw();
-	    }
-	  }
-	  
-	  public void addActor(Actor actor) {
-	    actor.world = this;
-	    actors.add(actor);
-	  }
-	  
-	  public List<Actor> getActors() {
-		  return actors;
-	  }
-	  
-	  public <T extends Actor> List<Actor> getActors(Class<T> subclass) {
-		  ArrayList<Actor> filtered = new ArrayList<>();
-		  for (Actor actor : actors)
-			  if (actor.getClass().equals(subclass))
-				  filtered.add(actor);
-		  return filtered;
-	  }
-	  
-	  @Override
-	  public void settings() {
-		  size(tempWidth, tempHeight);
-	  }
+
+    // -- variables --
+  
+    // array to hold actors
+    ArrayList<Actor> actors = new ArrayList<Actor>();
+    
+    // default world size if not given
+    static final int defaultWidth = 620;
+    static final int defaultHeight = 480;
+    
+    // non-default world dimensions
+    int tempWidth = defaultWidth;
+    int tempHeight = defaultHeight;
+    
+    // background image
+    Background background;
+    
+    //  -- constructors --
+    
+    // default constructor
+    public World() {
+      // default constructor
+    }
+    
+    // make world with dimensions
+    public World(int width, int height) {
+      tempWidth = width;
+      tempHeight = height;
+    }
+    
+    // make world with background and default dimensions
+    public World(Background background) {
+      this.background = background;
+    }
+    
+    // make world with background and given dimnnsions
+    public World(Background background, int width, int height) {
+      this.background = background;
+      
+      tempWidth = width;
+      tempHeight = height;
+    }
+    
+    // -- methods --
+    
+    // call this each update to trigger actor updates
+    public void display() {
+      background.display();
+      for (Actor actor : actors) {
+        actor.display();
+      }
+    }
+    
+    // add an actor to the list of updating actors
+    public void addActor(Actor actor) {
+      actor.world = this;
+      actors.add(actor);
+    }
+    
+    // remove an actor from the list of actors belonging to this world
+    public void removeActor(Actor actor) {
+      actor.world = null;
+      actors.remove(actor);
+    }
+    
+    // return an array of all actors
+    public ArrayList<Actor> getActors() {
+      return actors;
+    }
+    
+    // return an array of actors matching a given class
+    public <T extends Actor> ArrayList<Actor> getActors(Class<T> subclass) {
+      ArrayList<Actor> filtered = new ArrayList<Actor>();
+      for (Actor actor : actors)
+        if (actor.getClass().equals(subclass))
+          filtered.add(actor);
+      return filtered;
+    }
+    
+    // resizes world and changes background
+    public void applySettings() {
+      size(tempWidth, tempHeight);
+    }
 }
