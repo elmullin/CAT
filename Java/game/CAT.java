afad82b485d825828f5169b24a046053c21bcb81
dlkohler <dkohler42@gmail.com>
2017-06-03 14:05:20 -0700

    673763 - fixed bugs

--- Java/game/CAT.java
+++ Java/game/CAT.java
@@ -4,22 +4,23 @@ import processing.core.*;
 
 public class CAT extends PApplet{
 
-private World world;
+   private World world;
 
-public void settings(){
-    size(800, 600);
-}
+   public void settings(){
+       size(800, 600);
+   }
 
-public void setup(){
-    //scene setup
-    surface.setResizable(false);
-    world = new World(new Background("bg.png"));
+   public void setup(){
+      //scene setup
+      surface.setResizable(false);
+      world = new World(new Background("bg.png"));
     
-    PhysObject cat = new TabbyCat(50, 50);
+      PhysObject cat = new TabbyCat(50, 50);
     
-    world.addActor(cat);
-}
+      world.addActor(cat);
+   }
 
-public void draw(){
-    world.display();
+   public void draw(){
+      world.display();
+   }
 }
