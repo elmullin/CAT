13468c1fde6fc171b81e8f0484e2d1f965e9bd6b
dlkohler <dkohler42@gmail.com>
2017-06-03 14:10:52 -0700

    662687 - added ScoreZone.java

---
+++ Java/game/ScoreZone.java
@@ -0,0 +1,14 @@
+import processing.core.*;
+
+public class ScoreZone extends PhysObject {
+  
+  public ScoreZone(int x, int y){
+    super(false, x, y);
+  }
+  
+  public void scoreCat(TabbyCat scored){
+    getWorld().score.incrementCatsCorralled();
+    getWorld().removeActor(scored);
+  }//close scoreCat
+
+}
