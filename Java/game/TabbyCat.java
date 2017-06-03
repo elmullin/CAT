9806c657b71347fc3b391775ad44aeee73283cee
dlkohler <dkohler42@gmail.com>
2017-06-03 14:19:05 -0700

    662687 - replaced java standard library functions with java functions

---
+++ Java/game/TabbyCat.java
@@ -0,0 +1,73 @@
+package game;
+
+import processing.core.*;
+
+public class TabbyCat extends PhysObject {
+   /*
+    * Constants
+    */
+   private static final String SOUND_1 = "assets/Cat_Meow_1.mp3"
+   private static final String SOUND_2 = "assets/Cat_Meow_2.mp3"
+   private static final String SOUND_3 = "assets/Cat_Meow_3.mp3"
+   private static final String SOUND_4 = "assets/Cat_Meow_4.mp3"
+   private static final String SOUND_5 = "assets/Cat_Meow_5.mp3"
+   private static final String SOUND_6 = "assets/Cat_Meow_6.mp3"
+   private static final String SOUND_7 = "assets/Cat_Meow_7.mp3"
+   private static final String SOUND_8 = "assets/Cat_Purr_1.mp3"
+
+
+   /*
+    * Fields
+    */
+   private Music catSounds;
+   private double startTime;
+   private int waitTime;
+
+
+   // -------------------------------------------------------------------------
+
+   public TabbyCat(PImage image, int x, int y){
+      super(image, true, x, y);
+      catSounds = new Music();
+      markTime = millis();
+      waitTime = random(5000) + 10000;
+   }
+
+   // -------------------------------------------------------------------------
+   private void makeNoise() {
+      if (System.currentTimeMillis >= markTime + waitTime) {
+         switch (rng.nextInt(8) + 1) {
+            case 1:
+               catSounds.switchTrack(SOUND_1, false);
+               break;
+            case 2:
+               catSounds.switchTrack(SOUND_2, false);
+               break;
+            case 3:
+               catSounds.switchTrack(SOUND_3, false);
+               break;
+            case 4:
+               catSounds.switchTrack(SOUND_4, false);
+               break;
+            case 5:
+               catSounds.switchTrack(SOUND_5, false);
+               break;
+            case 6:
+               catSounds.switchTrack(SOUND_6, false);
+               break;
+            case 7:
+               catSounds.switchTrack(SOUND_7, false);
+               break;
+            case 8:
+               catSounds.switchTrack(SOUND_8, false);
+               break;
+         }
+         startTime = millis();
+         waitTime = random(5000) + 11000;
+      }
+   }
+
+   public void display() {
+      makeNoise();
+   }
+}
