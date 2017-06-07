d9fa8b8fef654811855d9dd7d9dc8196e2bb26bd
dlkohler <dkohler42@gmail.com>
2017-06-06 17:22:42 -0700

    673897 - added Zoe Cagle's test suite

---
+++ Java/test/TestSuiteZcagle.java
@@ -0,0 +1,14 @@
+/*
+ * Dylan Kohler
+ */
+
+package test;
+
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestPlayerKeyReleased.class })
+
+public class TestSuiteZcagle {
+
+}
