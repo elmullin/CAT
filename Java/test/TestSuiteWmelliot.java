def33e1572bf63b312a1aee4f5b7d5e3beafadcc
dlkohler <dkohler42@gmail.com>
2017-06-06 20:20:43 -0700

    673897 - renamed world tests

---
+++ Java/test/TestSuiteWmelliot.java
@@ -0,0 +1,14 @@
+/*
+ * William Elliott
+ */
+
+package test;
+
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestPlayerKeyReleased.class })
+
+public class TestSuiteWmelliot {
+
+}
